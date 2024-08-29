443c432ce7c96e716e6d741cf6eb415fd88a9342
In Tae Chung <intae29@gmail.com>
2024-08-29 19:59:11 -0400

    testing out geomapping with mapbox

--- app.js
+++ app.js
@@ -2,6 +2,18 @@ const request = require('postman-request')
 require('dotenv').config()
 
 const weatherKey = process.env.weatherStackAPI
+const mapboxKey = process.env.mapboxAPI
+
+// Geocoding
+// takes an address and gives back the Lat/Long
+
+const geomapUrl = 'https://api.mapbox.com/search/geocode/v6/forward?q=Toronto&access_token=' + mapboxKey + '&limit=1'
+
+request({url: geomapUrl, json: true}, (error, response) => {
+    console.log('Location: ' + response.body.features[0].properties.full_address + '.\n' + 
+    'Latitude: ' + response.body.features[0].properties.coordinates.latitude + '.\n' +
+    'Longitude: ' + response.body.features[0].properties.coordinates.longitude + '.\n')
+})
 
 const url = 'https://api.weatherstack.com/current?access_key=' + weatherKey + '&query=Toronto&units=m'
 
