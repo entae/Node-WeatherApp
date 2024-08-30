f9d39b0725e71e380c8e2cd78cf91c850a542808
In Tae Chung <intae29@gmail.com>
2024-08-29 20:17:32 -0400

    added error handling

--- app.js
+++ app.js
@@ -7,23 +7,39 @@ const mapboxKey = process.env.mapboxAPI
 // Geocoding
 // takes an address and gives back the Lat/Long
 
-const geomapUrl = 'https://api.mapbox.com/search/geocode/v6/forward?q=Toronto&access_token=' + mapboxKey + '&limit=1'
+const geocodeUrl = 'https://api.mapbox.com/search/geocode/v6/forward?q=Toronto&access_token=' + mapboxKey + '&limit=1'
 
-request({url: geomapUrl, json: true}, (error, response) => {
-    console.log('Location: ' + response.body.features[0].properties.full_address + '.\n' + 
-    'Latitude: ' + response.body.features[0].properties.coordinates.latitude + '.\n' +
-    'Longitude: ' + response.body.features[0].properties.coordinates.longitude + '.\n')
+request({url: geocodeUrl, json: true}, (error, response) => {
+    if (error) {
+        console.log('Unable to connect to location services')
+    } else if (!response.body.features[0]) {
+        console.log('Unable to find location. Try another search')
+    } else {
+        const location = response.body.features[0].properties.full_address
+        const latitude = response.body.features[0].properties.coordinates.latitude
+        const longitude = response.body.features[0].properties.coordinates.longitude
+    
+        console.log('Location: ' + location + '.\n' + 
+        'Latitude: ' + latitude + '.\n' +
+        'Longitude: ' + longitude + '.\n')
+    }
 })
 
-const url = 'https://api.weatherstack.com/current?access_key=' + weatherKey + '&query=Toronto&units=m'
+const weatherUrl = 'https://api.weatherstack.com/current?access_key=' + weatherKey + '&query=Toronto&units=m'
 
 // units
 // m = metric system
 // f = Fahrenheit system
 // s = scientific system (Kelvin)
 
-request({ url: url, json: true }, (error, response) => {
-    console.log('Weather condition: ' + response.body.current.weather_descriptions[0] + '. It is currently ' + response.body.current.temperature + ' degrees Celsius out. It feels like ' + response.body.current.feelslike + ' degrees Celsius out.')
+request({ url: weatherUrl, json: true }, (error, response) => {
+    if (error) {
+        console.log('Unable to connect to weather service')
+    } else if (response.body.error) {
+        console.log('Unable to find location')
+    } else {
+        console.log('Weather condition: ' + response.body.current.weather_descriptions[0] + '. It is currently ' + response.body.current.temperature + ' degrees Celsius out. It feels like ' + response.body.current.feelslike + ' degrees Celsius out.')
+    }
 })
 
 // Geocoding
